e66f70e124648685a856161a2883ee16d981fbf5
AhmedFitoh <ahmed.fitoh.zakrya@protonmail.com>
2025-02-07 14:47:20 +0200

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by AhmedFitoh on 2/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
